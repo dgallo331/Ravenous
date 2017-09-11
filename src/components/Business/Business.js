b8edcf1b7618e8b3d5adbcdf2d47f58cc123b76a
dgallo331 <30381026+dgallo331@users.noreply.github.com>
2017-09-11 19:25:28 -0400

    1.4

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -4,21 +4,21 @@ import "./Business.css";
 class Business extends React.Component{
   render(){
     return (
-      <div className="{this.props.business.Business}">
+      <div className="{business}">
         <div className="{this.props.business.image-container}">
-          <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
+          <img src={this.props.business.imageSrc} alt=''/>
         </div>
-        <h2>MarginOtto Pizzeria</h2>
+        <h2>{this.props.business.name}</h2>
         <div className="{this.props.business.Business-information}">
           <div className="{this.props.business.Business-address}">
-            <p>1010 Paddington Way</p>
-            <p>Bordertown</p>
-            <p>NY 10101</p>
+            <p>{this.props.business.address}</p>
+            <p>{this.props.business.city + ' ' + this.props.business.state}</p>
+            <p> {this.props.business.zipCode}</p>
           </div>
           <div className="{this.props.business.Business-reviews}">
-            <h3>ITALIAN</h3>
-            <h3 className="{this.props.business.rating}">4.5 stars}</h3>
-            <p>90 reviews</p>
+            <h3>{this.props.business.categories}</h3>
+            <h3 className="{this.props.business.rating}">{this.props.business.rating} stars</h3>
+            <p>{this.props.business.reviewCount} reviews</p>
           </div>
         </div>
       </div>
---
+++ src/util/Yelp.js
@@ -0,0 +1,43 @@
+const clientId = 'P4tSZPeEeDgVWaJH5hwwAA';
+const secret = 'Yt54Q5J2NZde6mzYBDbOSG6bkGxWaBuRDgKnzVKunkW5jX0FvOVaAnpuYyy6pIlb';
+let accessToken = '';
+
+export const Yelp = {
+  getAccessToken() {
+    if (accessToken) {
+      return new Promise(resolve => resolve(accessToken));
+    }
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=` + clientId + `&client_secret=` + secret,
+      {method:'POST'}).then(response => {
+        return response.json();
+      }).then(jsonResponse => {
+        accessToken = jsonResponse.access_token;
+    });
+  },
+  search(term, location, sortBy) {
+    return Yelp.getAccessToken().then(() => {
+      return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=` + term + `&location=` + location + `&sort_by=` + sortBy,
+      {headers: {Authorization: `Bearer ${accessToken}`}
+      });
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => (
+          {
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            categories: business.categories,
+            rating: business.rating,
+            reviewCount: business.review_count
+          }
+        ))
+      }
+    });
+  }
+}
